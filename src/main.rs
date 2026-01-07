99f6dd259517c1f7c9060aa4c12d717fe68233c3
AlaLit <alanlitv1230@gmail.com>
2026-01-07 21:11:37 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
 fn main() {
-    println!("Hello, world!");
+    let time = SystemTime::now();
+let duration = time.duration_since(UNIX_EPOCH).unwrap();
+let seconds = duration.as_secs();
+let nanos = duration.subsec_nanos();
+
+// Ручне форматування
+let days = seconds / 86400;
+let hours = (seconds % 86400) / 3600;
+let minutes = (seconds % 3600) / 60;
+let secs = seconds % 60;
+
+println!("{} днів {} годин {} хвилин {} секунд {} наносекунд", 
+         days, hours, minutes, secs, nanos);
 }

c1ebaebf2409f2f3fabd56ca70f661cf72a2c376
AlaLit <alanlitv1230@gmail.com>
2026-01-15 23:18:12 +0200

    add function
    add maseage

--- src/vidgets.rs
+++ src/vidgets.rs
@@ -1,6 +1,10 @@
 use fltk::{button::Button, prelude::*};
 use fltk::enums::Color;
 use fltk::enums::FrameType;
+use fltk::{
+    app, window::Window, frame::Frame,
+    app::Sender, app::Receiver, app::channel,
+};
 
 pub struct ButtonConfig<'a> {
     pub x: i32,
@@ -24,4 +28,36 @@ where
     btn.set_callback(move |_| callback());
     btn.set_frame(FrameType::RoundedBox);
     btn
+}
+
+
+#[derive(Clone, Copy)]
+pub enum MsgResult {
+    Ok,
+    Cancel,
+}
+
+pub fn message_confirm(text: &str) -> MsgResult {
+    let (s, r): (Sender<MsgResult>, Receiver<MsgResult>) = channel();
+
+    let mut win = Window::new(400, 300, 360, 160, "Підтвердження");
+    win.set_color(Color::from_rgb(30, 30, 30));
+
+    let mut label = Frame::new(20, 30, 320, 60, text);
+    label.set_label_color(Color::White);
+
+    let mut ok = Button::new(60, 100, 100, 35, "OK");
+
+    ok.set_callback(move |_| s.send(MsgResult::Ok));
+    cancel.set_callback(move |_| s.send(MsgResult::Cancel));
+
+    win.end();
+    win.show();
+
+    while let Some(msg) = r.recv() {
+        win.hide();
+        return msg;
+    }
+
+    MsgResult::Cancel
 }
